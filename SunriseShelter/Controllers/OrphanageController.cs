7977d1a30bcaae3a83012fc8898e6133eb5f47ee
ac121658 <ac121658@avcol.school.nz>
2025-07-31 15:14:29 +1200

    Began centering my table (Orphanage)

--- SunriseShelter/Controllers/OrphanageController.cs
+++ SunriseShelter/Controllers/OrphanageController.cs
@@ -54,7 +54,7 @@ namespace SunriseShelter.Controllers
                     orphanages = orphanages.OrderBy(o => o.Name);
                     break;
             }
-            int pageSize = 8;
+            int pageSize = 12;
             return View(await PaginatedList<Orphanage>.CreateAsync(orphanages.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
 
